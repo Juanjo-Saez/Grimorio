1cf7fbec582ed33266a096c17edd03d380f0b742
Juanjo-Saez <juanjosaezblanco@gmail.com>
2025-03-28 18:42:55 +0100

    create user tables

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'grimorio'),
             'username' => env('DB_USERNAME', 'wizard'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'wizard'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
--- database/migrations/2025_03_27_152712_create_users_table.php
+++ database/migrations/2025_03_27_152712_create_users_table.php
@@ -12,13 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id(); // Primary Key autoincremental
+            $table->string('username'); // Nombre de usuario
+            $table->string('email')->unique(); // Email único
+            $table->string('password'); // Contraseña encriptada
+            $table->timestamps(); // created_at y updated_at
         });
     }
 
